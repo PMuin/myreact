d67bd1be43be39a3f1d18cbd33bad64b6d5c0f1c
PMuin <99225997+PMuin@users.noreply.github.com>
2022-07-01 07:38:42 +0200

    react project

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+    return( 
+
+        <nav className="navbar bg-dark text-light mb-5">
+            <div className="container-fluid">
+                <h3 className="mx-auto">Weather Prediction</h3>
+            </div>
+
+        </nav>
+    );
+}
+
+export default Navbar;
